8da7c4b58bd6b16d3c9cfb618107918834846cc7
Paul King <cronald@gmail.com>
2012-05-12 13:45:25 +0100

    Test

---
+++ js/mozhackday.js
@@ -0,0 +1,34 @@
+
+/*
+
+var request = navigator.mozApps.getSelf();
+request.onsuccess = function() {
+  if (request.result) {
+    alert("OK");
+  } else {
+    alert("FUCK");
+  }
+}
+request.onerror = function() {
+  alert('Error checking installation status: ' + this.error.message);
+}
+
+*/
+
+alert(navigator.mozCamera);
+
+
+/*
+var request = navigator.mozApps.install("http://mozhackday.gaiamobile.org");
+
+request.onsuccess = function() {
+  alert("yes");
+}
+
+request.onerror = function(e) {
+  alert("no");
+}
+
+
+*/
+
