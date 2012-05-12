6f6e84898b5aee6614bcd38b27599f668ee9b767
Paul King <cronald@gmail.com>
2012-05-12 16:03:23 +0100

    Tek pics and shove em into t'canvas

--- js/mozhackday.js
+++ js/mozhackday.js
@@ -1,34 +1,33 @@
 
-/*
+window.addEventListener('DOMContentLoaded', function CameraInit() {
+  var config = {
+    height: document.body.clientHeight,
+    width: document.body.clientWidth,
+    camera: 0
+  };
 
-var request = navigator.mozApps.getSelf();
-request.onsuccess = function() {
-  if (request.result) {
-    alert("OK");
-  } else {
-    alert("FUCK");
+  var viewfinder = document.getElementById("viewfinder");
+
+  viewfinder.style.width = config.width + "px";
+  viewfinder.style.height = config.height + "px";
+  viewfinder.style.MozTransform = "rotate(90deg)";
+
+  var button = document.getElementById("capture_button");
+  button.addEventListener("click", function() {
+    var w = viewfinder.videoWidth;
+    var h = viewfinder.videoHeight;
+    var canvas = document.getElementById("capture");
+    canvas.width = w;
+    canvas.height = h;
+
+    viewfinder.style.display = "none";
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(viewfinder, 0, 0, w, h);
+  });
+
+  if(navigator.mozCamera) {
+    viewfinder.src = navigator.mozCamera.getCameraURI(config);
   }
-}
-request.onerror = function() {
-  alert('Error checking installation status: ' + this.error.message);
-}
-
-*/
-
-alert(navigator.mozCamera);
-
-
-/*
-var request = navigator.mozApps.install("http://mozhackday.gaiamobile.org");
-
-request.onsuccess = function() {
-  alert("yes");
-}
-
-request.onerror = function(e) {
-  alert("no");
-}
-
-
-*/
+});
 
